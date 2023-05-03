633b72398c8b47f1efa5d5196e18df38cf43629e
Denis Smorgunov <d.smorgunov@inspector-cloud.com>
2023-05-03 15:58:42 +0700

    chore: Configuration eslint and prettier.

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'all',
+  semi: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+}
